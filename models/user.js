ab031d31cb84ef745e2251a85419297cd2b8d2c7
mtownsend509 <mgtownsend509@gmail.com>
2023-02-18 13:56:34 -0800

    completed user schema?

--- models/user.js
+++ models/user.js
@@ -1,1 +1,46 @@
 const { Schema, model, Types } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [
+                /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+                `Need a valid email address`
+        ]
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'thought'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }],
+    },
+
+    {
+        toJson: {
+            virtuals: true,
+            getters: true
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(() => {
+    return this.friends.length;
+});
+
+const user = model('user', userSchema);
+
+module.exports = user;
+
